5613720a0a75ba41f9db8aa878ce2f61fef752d8
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-06-20 18:18:15 +0300

    Tests for backup files

--- tests/file.test.ts
+++ tests/file.test.ts
@@ -51,7 +51,7 @@ describe('POST /file/:fileId', () => {
       .set('Authorization', `Bearer ${TEST_TOKEN}`)
       .expect(StatusCodes.LENGTH_REQUIRED, done);
   });
-  it('should return 413 Payload Too Large if no File-Size header', done => {
+  it('should return 413 Payload Too Large if no File-Size is too large', done => {
     request(app).post('/file/file-for-upload')
       .set('Authorization', `Bearer ${TEST_TOKEN}`)
       .set('File-Size', `${config.STORAGE_SIZE_LIMIT + 1}`)
@@ -82,6 +82,28 @@ describe('POST /file/:fileId', () => {
         done();
       });;
   });
+  it('should create backup file if already exists', done => {
+    const existingFileContent = 'Existing file content.';
+    createTestFile('existing-file', existingFileContent);
+    const buffer = Buffer.from('New file content.', 'utf8')
+    request(app).post('/file/existing-file')
+      .set('Authorization', `Bearer ${TEST_TOKEN}`)
+      .set('File-Size', `${buffer.length}`)
+      .send(buffer)
+      .expect(StatusCodes.OK)
+      .end((err, res) => {
+        if (err) return done(err);
+        assert.equal(res.body.used, buffer.length);
+        assert.equal(
+          storageSpace.reservedForBackups,
+          Buffer.from(existingFileContent).length
+        );
+        assert(fs.existsSync(
+          path.join(config.STORAGE_PATH, 'existing-file.bak')
+        ));
+        done();
+      });;
+  });
 });
 
 describe('DELETE /file/:fileId', () => {
@@ -94,7 +116,7 @@ describe('DELETE /file/:fileId', () => {
       .set('Authorization', `Bearer ${TEST_TOKEN}`)
       .expect(StatusCodes.NOT_FOUND, done);
   });
-  it('should return 200 OK and delete file', done => {
+  it('should return 200 OK and create backup file', done => {
     createTestFile('file-for-delete', 'File content.');
     request(app).delete('/file/file-for-delete')
       .set('Authorization', `Bearer ${TEST_TOKEN}`)
@@ -104,17 +126,22 @@ describe('DELETE /file/:fileId', () => {
         assert(!fs.existsSync(
           path.join(config.STORAGE_PATH, 'file-for-delete')
         ));
+        assert(fs.existsSync(
+          path.join(config.STORAGE_PATH, 'file-for-delete.bak')
+        ));
         done();
       });
   });
   it('should return correct storage space', done => {
     createTestFile('file-for-delete', 'File content.');
+    const usedSpace = storageSpace.used;
     request(app).delete('/file/file-for-delete')
       .set('Authorization', `Bearer ${TEST_TOKEN}`)
       .expect(StatusCodes.OK)
       .end((err, res) => {
         if (err) return done(err);
         assert.equal(res.body.used, 0);
+        assert.equal(storageSpace.reservedForBackups, usedSpace);
         done();
       });
   });
